d0d57395e568b5105173da08de021e621f9dc04b
Jiawei Chen <jich7678@colorado.edu>
2023-10-18 15:16:47 -0600

    Initial Commit

---
+++ App Recreate/App_RecreateApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_RecreateApp.swift
+//  App Recreate
+//
+//  Created by Sam Chen on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct App_RecreateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
